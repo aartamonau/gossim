cbc9f2737674657a0fed697230f21c2a59382ee3
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-03-24 00:59:09 -0700

    Reexport useful stuff from Logging module in Gossim.

--- src/Gossim.hs
+++ src/Gossim.hs
@@ -2,6 +2,7 @@ module Gossim
        ( module Gossim.Internal.Types
        , module Gossim.Internal.Agent
        , module Gossim.Internal.Random
+       , module Gossim.Internal.Logging
        ) where
 
 import Gossim.Internal.Types (AgentId, RumorId, Rumor(rumorId))
@@ -13,3 +14,8 @@ import Gossim.Internal.Random (MonadRandom,
                                randomInt, randomRInt, randomDouble,
                                randomBool, randomMaybe, randomMaybeM,
                                pick, pickUniformly)
+import Gossim.Internal.Logging (Level(Trace, Debug, Info, Warning, Error, Fatal),
+                                Only(Only),
+                                logM, traceM, debugM, infoM,
+                                warningM, errorM, fatalM,
+                                scope, format)
